cd7fd13459c67f2be3e59a6a6a60bde00f359c1b
Tirklee <swdz0412@qq.com>
2023-07-04 12:25:22 +0800

    修改自述以及图片题注

---
+++ word/6.新增图片并添加题注.vb
@@ -0,0 +1,8 @@
+Sub insertImageAndCaption()
+    Selection.InsertBreak Type:=wdLineBreak
+    Selection.InlineShapes.AddPicture FileName:= _
+        "D:\360MoveData\Users\Administrator\Desktop\asd.png", LinkToFile:=False, _
+        SaveWithDocument:=True
+    Selection.InsertBreak Type:=wdLineBreak
+    Selection.InsertCaption Label:="图", TitleAutoText:="阿松大", Title:="阿松大", Position:=wdCaptionPositionBelow, ExcludeLabel:=0
+End Sub
